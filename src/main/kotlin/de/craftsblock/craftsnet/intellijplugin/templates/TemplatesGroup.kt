b46bb36812cacfec7ab3063489c207a4952c9fdb
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2026-04-24 20:02:55 +0200

    fix: Replace missing toDistinctTypedArray (#18)

--- src/main/kotlin/de/craftsblock/craftsnet/intellijplugin/templates/TemplatesGroup.kt
+++ src/main/kotlin/de/craftsblock/craftsnet/intellijplugin/templates/TemplatesGroup.kt
@@ -1,11 +1,11 @@
 package de.craftsblock.craftsnet.intellijplugin.templates
 
-import ai.grazie.utils.toDistinctTypedArray
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import de.craftsblock.craftsnet.intellijplugin.templates.impl.*
+import de.craftsblock.craftsnet.intellijplugin.uitls.toDistinctTypedArray
 import de.craftsblock.craftsnet.intellijplugin.uitls.versioning.CraftsNetVersionUtils
 
 private val DIALOG_TEMPLATES: Set<CustomAction> = setOf(
